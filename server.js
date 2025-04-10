52da97eac9fd3bcb90fde983b3e27137e9c5e7aa
Julia Vrn <example@myemail.com>
2025-04-10 12:15:38 +0200

    add week 14 files

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
-// Указываем папки для статических файлов
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-// Отправка файла index.html при обращении к корневому маршруту
+
 app.get('/', (req, res) => {
     
     //res.send('hello');
